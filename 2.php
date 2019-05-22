2f8586f46170a4cca4c9e7e8ad79cfd682d53870
Your Name <your@example.com>
2019-05-22 08:45:10 +0800

    a

---
+++ 2.php
@@ -0,0 +1,19 @@
+<?php 
+
+$str = "student. a am I";
+
+$arr = [];
+
+$len = strlen($str);
+
+
+for ($i=0; $i < $len; $i++) { 
+	if (trim($i)) {
+		$arr = $i;
+	}
+}
+
+echo "I am a student.";
+
+
+ ?>
